b30276742e0eb164308f63471f1f93c867fdda2c
RICARDO DIDIMO <ricardo.didimo@fique.online>
2025-01-14 17:59:16 -0300

    Feature: aded typed result to moviedb API return

--- Models/MoviePickRoom.cs
+++ Models/MoviePickRoom.cs
@@ -7,11 +7,12 @@ public class MoviePickRoom
     public required List<Player> PlayersConnected { get; set; }
     public int PlayersConnectedTotal => this.PlayersConnected.Count;
     public required List<Player> PlayersConfirmed { get; set; }
+    public int PlayersConfirmedTotal => this.PlayersConnected.Count;
     public bool CanBeginRound => this.PlayersConnected.Count == PlayersConfirmed.Count;
     public int CurrentRound { get; set; }
     public required bool AllowUserPreferences { get; set; }
     public required FilterPreferences RoomPreferences { get; set; }
     public int MaxPlayers { get; set; }
     public int MaxSuggestions { get; set; }
-    public SuggestionsResult? Suggestions {get; set;}
+    public SuggestionsResult? Suggestions { get; set; }
 }
--- Models/SuggestionsResult.cs
+++ Models/SuggestionsResult.cs
@@ -3,5 +3,5 @@ namespace cinema.Models;
 public record SuggestionsResult
 {
     public int page { get; set; }
-    public required List<object> results { get; set; }
+    public required List<Movie> results { get; set; }
 }
