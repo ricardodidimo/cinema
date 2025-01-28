757c39457b97f47f08a8210c54a29b0c220dd175
RICARDO DIDIMO <ricardo.didimo@fique.online>
2025-01-28 09:04:30 -0300

    Feat: Added code to delete active room when it reaches zero participants on 'LeaveRoom'

--- Events/RoomHub/LeaveRoom/LeaveRoom.cs
+++ Events/RoomHub/LeaveRoom/LeaveRoom.cs
@@ -5,23 +5,30 @@ using FluentResults;
 namespace cinema.Events.RoomHub.LeaveRoom;
 
 
-public class LeaveRoom(IRoomRepository _RoomRepository) : ILeaveRoomEvent
+public class LeaveRoom(IRoomRepository _roomRepository) : ILeaveRoomEvent
 {
     public async Task<IResult<MoviePickRoom>> Exec(LeaveRoomRequest request, Player player)
     {
-        var roomFindOp = await _RoomRepository.FindByIdentifier(request.RoomCode);
+        var roomFindOp = await _roomRepository.FindByIdentifier(request.RoomCode);
         var roomEntry = roomFindOp.Value;
         if (roomFindOp.IsFailed || roomEntry is null)
         {
             return await Task.Run(() => Result.Fail<MoviePickRoom>(roomFindOp.Errors));
         }
 
-        var leaveRoom = await _RoomRepository.RemovePlayer(roomEntry, player);
+        var leaveRoom = await _roomRepository.RemovePlayer(roomEntry, player);
+        MoviePickRoom room = leaveRoom.Value;
         if (leaveRoom.IsFailed)
         {
             return Result.Fail<MoviePickRoom>(leaveRoom.Errors);
         }
 
-        return Result.Ok(leaveRoom.Value);
+        bool isRoomEmpty = room.PlayersConnectedTotal is 0;
+        if (isRoomEmpty)
+        {
+            return await _roomRepository.Delete(room);
+        }
+
+        return Result.Ok(room);
     }
 }
